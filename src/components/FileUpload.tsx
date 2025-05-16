781e50fab32985358f808528fc5029c88ee9406a
Raphael Ramos <raphael.opensource@gmail.com>
2025-05-16 14:17:18 -0300

    feat: read file on the client side

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,6 +1,21 @@
 function FileUpload() {
+
+    let fileReader : FileReader;
+
+    const handleFileChosen = (file : File) => {
+        fileReader = new FileReader();
+        fileReader.onloadend = (e) => {
+            const content = fileReader.result;
+            console.log(content);
+        };
+        fileReader.readAsText(file);
+    }
+
     return (
-        <><p>File handler</p></>
+        <>
+            <p>File handler</p>
+            <input type="file" accept=".txt" onChange={e => handleFileChosen(e.target.files[0])}/>
+        </>
     );
 }
 
