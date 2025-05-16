cf9ca63d863c747a28cecdd0109f0cc841bb4a10
Raphael Ramos <raphael.opensource@gmail.com>
2025-05-16 13:58:17 -0300

    avoid file server

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,44 +1,6 @@
-import { useEffect, useState } from "react";
-
 function FileUpload() {
-
-    const [greeting, setGreeting] = useState('');
-    const [file, setFile] = useState(null);
-
-    useEffect(() => {
-        setTimeout(async () => {
-            const res = await fetch("http://localhost:3000/", {
-                method: 'GET'
-            });
-            const json = await res.json();
-            setGreeting(json.message);
-        }, 1000);
-    }, []);    
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('file', file);
-        try {
-            const res = await fetch("http://localhost:3000/file", {
-                method: "POST",
-                body: formData
-            });
-            const data = await res.json();
-            console.log(data);
-        } catch (err) {
-            console.error("Erro no upload:", err);
-        }
-    }
-
     return (
-        <>
-            <p>{greeting}</p>
-            <form onSubmit={handleSubmit} encType="multipart/form-data" method="POST">
-                <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-                <button type="submit">Carregar</button>
-            </form>
-        </>
+        <><p>File handler</p></>
     );
 }
 
