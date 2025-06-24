800720a4f26f6a53842449d8eed23fc531474247
Raphael Ramos <raphael.opensource@gmail.com>
2025-06-23 22:37:27 -0300

    fix: file upload not used exception

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import FileUpload from './components/FileUpload'
+// import FileUpload from './components/FileUpload'
 import { FormDialog } from './components/FormDialog'
 
 function App() {
